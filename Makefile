f3ca52245045a488f5430eb031dc954d4ead509a
Siwoo Jeon <me@miganbox.com>
2026-09-04 23:35:15 +0900

    feat: add libgit2 and print commit logs

--- Makefile
+++ Makefile
@@ -2,30 +2,43 @@ CC := clang
 CFLAGS := -Wall -Wextra -Iinclude -MMD -MP
 DEBUG ?= 0
 OBJDIR := obj
+UNAME := $(shell uname -s)
+LDFLAGS :=
+LDLIBS = -lgit2
 
 ifeq ($(DEBUG), 1)
-    CFLAGS += -O0 -fsanitize=address,undefined -g
+    SANFLAGS := -fsanitize=address,undefined
+    CFLAGS += -O0 $(SANFLAGS) -g
+    LDFLAGS += $(SANFLAGS)
 else
     CFLAGS += -O2
+endif
+
+ifeq ($(UNAME), Darwin)
+	CFLAGS += -I/opt/homebrew/include
+	LDFLAGS += -L/opt/homebrew/lib
 endif
 
 TARGET := main.out
 
 SRCS := $(wildcard *.c)
 OBJS := $(SRCS:%.c=$(OBJDIR)/%.o)
+DEPS := $(OBJS:.o=.d)
 
 all: $(TARGET)
 
 $(TARGET): $(OBJS)
-	$(CC) $(CFLAGS) -o $@ $^
+	@$(CC) $(CFLAGS) $(LDFLAGS) -o $@ $^ $(LDLIBS)
 
 $(OBJDIR)/%.o: %.c | $(OBJDIR)
-	$(CC) $(CFLAGS) -c $< -o $@
+	@$(CC) $(CFLAGS) -c $< -o $@
 
 $(OBJDIR):
 	@mkdir -p $(OBJDIR)
 
 clean:
 	@rm -rf $(OBJDIR) $(TARGET)
+
+-include $(DEPS)
 
 .PHONY: all clean
--- main.c
+++ main.c
@@ -1,7 +1,72 @@
-#include "test.h"
+// SPDX-License-Identifier: GPL-2.0-only
+// Copyright 2026 Siwoo Jeon
+#include <git2.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(void)
+int main(int argc, char *argv[])
 {
-	print_hello();
-	return 0;
+	if (argc <= 1) {
+		fprintf(stderr, "git repository must be provided!\n");
+		return 1;
+	}
+
+	int err = 0;
+	const git_error *git_err = NULL;
+
+	struct git_repository *repo = NULL;
+	struct git_revwalk *walk = NULL;
+	git_oid oid;
+
+	git_commit *commit = NULL;
+	const struct git_signature *author = NULL;
+
+	err = git_libgit2_init();
+	if (err < 0) {
+		goto out;
+	}
+
+	err = git_repository_open(&repo, argv[1]);
+	if (err != 0) {
+		goto out;
+	}
+
+	err = git_revwalk_new(&walk, repo);
+	if (err != 0) {
+		goto out_repo_free;
+	}
+
+	err = git_revwalk_push_head(walk);
+	if (err != 0) {
+		goto out_revwalk_free;
+	}
+
+	while (!git_revwalk_next(&oid, walk)) {
+		if (git_commit_lookup(&commit, repo, &oid) != 0) {
+			continue;
+		}
+
+		author = git_commit_author(commit);
+
+		printf("commit %s\n", git_oid_tostr_s(git_commit_id(commit)));
+		printf("Author:      %s <%s>\n", author->name, author->email);
+		printf("Timestamp:   %lld\n", author->when.time);
+		printf("\n   %s\n", git_commit_message(commit));
+
+		git_commit_free(commit);
+	}
+
+out_revwalk_free:
+	git_revwalk_free(walk);
+out_repo_free:
+	git_repository_free(repo);
+out:
+	if (err != 0) {
+		git_err = git_error_last();
+		fprintf(stderr, "%s\n", git_err->message);
+	}
+
+	git_libgit2_shutdown();
+
+	return err;
 }
