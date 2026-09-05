23ec0e9b4d3d1ac627333295e54ac763eed524b5
Siwoo Jeon <me@miganbox.com>
2026-09-05 17:49:10 +0900

    feat: add basic and basic cgi logic

--- main.c
+++ main.c
@@ -1,72 +1,26 @@
 // SPDX-License-Identifier: GPL-2.0-only
 // Copyright 2026 Siwoo Jeon
-#include <git2.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char *argv[])
+#define CONTENT_TYPE_HTML "Content-Type: text/html"
+#define SEPARATOR "\r\n\r\n"
+
+int main(void)
 {
-	if (argc <= 1) {
-		fprintf(stderr, "git repository must be provided!\n");
+	char *method = NULL;
+
+	method = getenv("REQUEST_METHOD");
+	if (method == NULL || strcmp(method, "GET") != 0) {
+		printf("%s%s", CONTENT_TYPE_HTML, SEPARATOR);
+		printf("<html><head><title>cgi</title></head><body><h1>WRONG "
+		       "METHOD</h1></body></html>\n");
 		return 1;
 	}
 
-	int err = 0;
-	const git_error *git_err = NULL;
-
-	struct git_repository *repo = NULL;
-	struct git_revwalk *walk = NULL;
-	git_oid oid;
-
-	git_commit *commit = NULL;
-	const struct git_signature *author = NULL;
-
-	err = git_libgit2_init();
-	if (err < 0) {
-		goto out;
-	}
-
-	err = git_repository_open(&repo, argv[1]);
-	if (err != 0) {
-		goto out;
-	}
-
-	err = git_revwalk_new(&walk, repo);
-	if (err != 0) {
-		goto out_repo_free;
-	}
-
-	err = git_revwalk_push_head(walk);
-	if (err != 0) {
-		goto out_revwalk_free;
-	}
-
-	while (!git_revwalk_next(&oid, walk)) {
-		if (git_commit_lookup(&commit, repo, &oid) != 0) {
-			continue;
-		}
-
-		author = git_commit_author(commit);
-
-		printf("commit %s\n", git_oid_tostr_s(git_commit_id(commit)));
-		printf("Author:      %s <%s>\n", author->name, author->email);
-		printf("Timestamp:   %lld\n", author->when.time);
-		printf("\n   %s\n", git_commit_message(commit));
-
-		git_commit_free(commit);
-	}
-
-out_revwalk_free:
-	git_revwalk_free(walk);
-out_repo_free:
-	git_repository_free(repo);
-out:
-	if (err != 0) {
-		git_err = git_error_last();
-		fprintf(stderr, "%s\n", git_err->message);
-	}
-
-	git_libgit2_shutdown();
-
-	return err;
+	printf("%s%s", CONTENT_TYPE_HTML, SEPARATOR);
+	printf("<html><head><title>cgi</title></head><body><h1>CORRECT "
+	       "METHOD</h1></body></html>\n");
+	return 0;
 }
